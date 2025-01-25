ed3d38e51892719606a7ee4a205be717d39d98d4
“richardcrn3rd” <richardcrn3rd@gmail.com>
2025-01-25 11:46:54 -0500

    Added additional files to SRC folder

---
+++ comics-in-context/src/RequestFormButton.js
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import StylesContext from "./StylesContext";
+
+export default function RequestFormButton() {
+    const buttonStyling = useContext(StylesContext)
+
+    return (
+        <div>
+            <button>
+                Click Here!
+            </button>
+            <input type="submit" value="Submit" style={buttonStyling} />
+        </div>
+    );
+}
